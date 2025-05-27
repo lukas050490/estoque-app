9e70d1e81e41ecefe7582286ca4fe268d71eee92
Lucas <lucas_alexandre05@hotmail.com>
2025-05-27 16:09:38 -0300

    alteração no style da tela de ProductsPage

--- src/pages/ProductsPage/styles.js
+++ src/pages/ProductsPage/styles.js
@@ -6,13 +6,15 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   width: 100%;
-  height: 100vh;
+  height: auto;
   min-height: 100vh;
-  padding-top: 20rem;
+  padding: 2rem 0 2rem 0;
   background: linear-gradient(to bottom, #1976d2 0%, #ffffff 100%);
-
+  overflow-x: hidden;
+  overflow-y: auto;
+  
   .btn-voltar {
     width: 100%;
   }
